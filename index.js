9e95375b9bd72d9cff309adaa26adabea1b9d4fa
madelinedulchin <madeline.dulchin@gmail.com>
2022-09-21 13:27:37 -0400

    commit tree ab856c1410518481510a1b106cec4ac315c6d63f

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector("#main")
+main.remove()
+//could have just done main.remove
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Madeline is the champion'
+
